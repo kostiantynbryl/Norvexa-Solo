650fbd268dd8dde75890964205e0093af42902e0
Kostiantyn Bryl <kostiantynbryl@gmail.com>
2026-08-07 17:04:01 +0300

    release: bump Norvexa Flow to 0.2.0-alpha01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.norvexa.flow"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0-alpha01"
+        versionCode = 2
+        versionName = "0.2.0-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true

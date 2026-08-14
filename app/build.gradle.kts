35eb31f0bbeafbfd4c57d9dad56cff63b2a3971c
Kostiantyn Bryl <kostiantynbryl@gmail.com>
2026-08-14 09:09:47 +0300

    release: bump version to 0.2.1-alpha01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.norvexa.flow"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0-alpha01"
+        versionCode = 3
+        versionName = "0.2.1-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
